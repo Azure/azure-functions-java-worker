6df56a3a4a8b05aaaa49c69157894323f68ca583
Junyi Yi <junyi@microsoft.com>
2017-06-14 18:57:10 -0700

    Introduce core interface project and restructure the project.

---
+++ azure-functions-java-core/src/main/java/com/microsoft/azure/serverless/functions/HttpRequest.java
@@ -0,0 +1,11 @@
+package com.microsoft.azure.serverless.functions;
+
+public class HttpRequest {
+    public HttpRequest() {
+    }
+
+    @Override
+    public String toString() {
+        return "My Test HttpRequest";
+    }
+}
