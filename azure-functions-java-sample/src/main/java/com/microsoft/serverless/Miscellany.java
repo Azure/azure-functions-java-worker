7ec992f6104dc0480aaf5d728a73ed57ff86a5a6
Junyi Yi <junyi@microsoft.com>
2017-08-02 18:18:21 -0700

    Update the sample code for demo.

--- azure-functions-java-sample/src/main/java/com/microsoft/serverless/Miscellany.java
+++ azure-functions-java-sample/src/main/java/com/microsoft/serverless/Miscellany.java
@@ -1,6 +1,7 @@
 package com.microsoft.serverless;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import com.microsoft.azure.serverless.functions.ExecutionContext;
@@ -10,8 +11,9 @@ import com.microsoft.azure.serverless.functions.annotation.Bind;
 
 public class Miscellany {
     public static String echo(String body, @Bind("message") OutputParameter<String> message) {
-        message.setValue("Hello, " + (body != null && !body.isEmpty() ? body : "[Unnamed]") + "!");
-        return message.getValue();
+        String result = String.format("Hello, %s!", (body != null && !body.isEmpty() ? body : "[Unnamed]"));
+        message.setValue(String.format("%s [generated at %2$tD %2$tT, by echo]", result, new Date()));
+        return result;
     }
 
     public static String echo(@Bind("name") String param, String body, @Bind("message") OutputParameter<String> message) {
@@ -21,15 +23,22 @@ public class Miscellany {
         return echo(String.join(" and ", targets), message);
     }
 
+    public static String heartbeat(ExecutionContext context) {
+        context.getLogger().info("Java Timer Trigger Function Executed.");
+        return String.format("I'm still alive :) [generated at %1$tD %1$tT, by heartbeat]", new Date());
+    }
+
     public static HttpResponseMessage upload(byte[] data) {
         return new HttpResponseMessage(202, data.length + " bytes");
     }
 
-    public static void heartbeat(ExecutionContext context) {
-        context.getLogger().info("Java Timer Trigger Function Executed.");
+    public static String listMessages(@Bind("storage") String storage, @Bind("result") OutputParameter<String[]> result) {
+        result.setValue(storage.split("\n"));
+        return String.format("[Last consumed at %1$tD %1$tT]", new Date());
     }
 
-    public static void handleMessage(ExecutionContext context) {
-        context.getLogger().info("Java Queue Trigger Function Executed.");
+    public static String handleMessage(@Bind("message") String message, @Bind("storage") String storage) {
+        message = String.format("%s [handled at %2$tD %2$tT]", message, new Date());
+        return storage + "\n" + message;
     }
 }
