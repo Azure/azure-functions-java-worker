7ad8f76a53914dc5683e567834c8e0172605105a
Kevin Zhao <kevinzha@microsoft.com>
2017-09-13 14:34:08 +0800

    support more bindings: ApiHubFile/DocumentDB/MobileTable/NotificationHub/SendGrid/Twilio

---
+++ azure-functions-java-core/src/main/java/com/microsoft/azure/serverless/functions/annotation/MobileTableInput.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for
+ * license information.
+ */
+
+package com.microsoft.azure.serverless.functions.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.PARAMETER)
+public @interface MobileTableInput {
+    String name();
+
+    String tableName();
+
+    String id();
+
+    String connection();
+
+    String apiKey();
+}
