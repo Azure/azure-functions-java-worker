7452ac5dea12fceee1f76a3938f18609adbf4601
Junyi Yi <junyi@microsoft.com>
2017-09-15 17:57:57 -0700

    Move samples and demos to a dedicated samples repository. (#14)

--- functions-coffee-order-demo/src/main/java/com/microsoft/serverless/coffeeorder/InventoryTableEntity.java
+++
@@ -1,9 +0,0 @@
-package com.microsoft.serverless.coffeeorder;
-
-import com.microsoft.azure.storage.table.TableServiceEntity;
-
-public class InventoryTableEntity extends TableServiceEntity {
-    public int getAmount() { return this.amount; }
-    public void setAmount(int amount) { this.amount = amount; }
-    private int amount;
-}
