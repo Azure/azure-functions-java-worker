9c045313fe5a28b5e4f21fa037c22b160f08eec9
Christopher Anderson <chrande@microsoft.com>
2018-02-21 16:24:14 -0800

    add EventGridTrigger support

---
+++ azure-functions-java-core/src/main/java/com/microsoft/azure/serverless/functions/annotation/EventGridTrigger.java
@@ -0,0 +1,42 @@
+/*
+[JsonProperty(PropertyName = "topic")]
+        public string Topic { get; set; }
+
+        [JsonProperty(PropertyName = "subject")]
+        public string Subject { get; set; }
+
+        [JsonProperty(PropertyName = "data")]
+        public JObject Data { get; set; }
+
+        [JsonProperty(PropertyName = "eventType")]
+        public string EventType { get; set; }
+
+        [JsonProperty(PropertyName = "publishTime")]
+        public DateTime PublishTime { get; set; }
+
+        [JsonProperty(PropertyName = "eventTime")]
+        public DateTime EventTime { get; set; }
+
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+*/
+
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for
+ * license information.
+ */
+
+package com.microsoft.azure.serverless.functions.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.PARAMETER, ElementType.METHOD})
+public @interface EventGridTrigger {
+    
+}
+
