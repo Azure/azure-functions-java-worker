7708e63573a9b34122b84fb901a43b908b2d511f
Christopher Anderson <chrande@microsoft.com>
2018-02-21 16:58:38 -0800

    Updated Definitoin enum

--- azure-functions-java-core/src/main/java/com/microsoft/azure/serverless/functions/annotation/EventGridTrigger.java
+++ azure-functions-java-core/src/main/java/com/microsoft/azure/serverless/functions/annotation/EventGridTrigger.java
@@ -1,26 +1,3 @@
-/*
-[JsonProperty(PropertyName = "topic")]
-        public string Topic { get; set; }
-
-        [JsonProperty(PropertyName = "subject")]
-        public string Subject { get; set; }
-
-        [JsonProperty(PropertyName = "data")]
-        public JObject Data { get; set; }
-
-        [JsonProperty(PropertyName = "eventType")]
-        public string EventType { get; set; }
-
-        [JsonProperty(PropertyName = "publishTime")]
-        public DateTime PublishTime { get; set; }
-
-        [JsonProperty(PropertyName = "eventTime")]
-        public DateTime EventTime { get; set; }
-
-        [JsonProperty(PropertyName = "id")]
-        public string Id { get; set; }
-*/
-
 /**
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See License.txt in the project root for
