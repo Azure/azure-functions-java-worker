a02ebf08314cc06d8ea6efe3f3d21025e3dd7ebf
Jonathan Giles <jonathan@jonathangiles.net>
2018-03-20 14:11:06 +1300

    A few improvements to the JavaDoc for Azure Functions / Java

--- azure-functions-java-core/src/main/java/com/microsoft/azure/serverless/functions/HttpResponseMessage.java
+++ azure-functions-java-core/src/main/java/com/microsoft/azure/serverless/functions/HttpResponseMessage.java
@@ -1,10 +1,51 @@
 package com.microsoft.azure.serverless.functions;
 
+/**
+ * An HttpResponseMessage instance is returned by Azure Functions methods that are triggered by an
+ * {@link com.microsoft.azure.serverless.functions.annotation.HttpTrigger}.
+ *
+ * @see com.microsoft.azure.serverless.functions.annotation.HttpTrigger
+ * @see HttpRequestMessage
+ * @param <T> The type of the body, as determined by the return type specified on the function itself.
+ * @since 1.0.0
+ */
 public interface HttpResponseMessage<T> {
+
+    /**
+     * Returns the status code set on the HttpResponseMessage instance.
+     * @return the status code set on the HttpResponseMessage instance.
+     */
     int getStatus();
+
+    /**
+     * Sets the status code on the HttpResponseMessage instance.
+     * @param status An HTTP status code representing the outcome of the HTTP request.
+     */
     void setStatus(int status);
+
+    /**
+     * Adds a (key,value) header to the response.
+     * @param key The key of the header value.
+     * @param value The value of the header value.
+     */
     void addHeader(String key, String value);
+
+    /**
+     * Returns a header value for the given key.
+     * @param key The key for which the header value is sought.
+     * @return Returns the value if the key has previously been added, or null if it has not.
+     */
     String getHeader(String key);
+
+    /**
+     * Returns the body of the HTTP response.
+     * @return the body of the HTTP response.
+     */
     T getBody();
+
+    /**
+     * Sets the body of the HTTP response.
+     * @param body The body of the HTTP response
+     */
     void setBody(T body);
 }
--- azure-functions-java-core/src/main/java/com/microsoft/azure/serverless/functions/annotation/HttpTrigger.java
+++ azure-functions-java-core/src/main/java/com/microsoft/azure/serverless/functions/annotation/HttpTrigger.java
@@ -11,9 +11,42 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * <p>The HttpTrigger annotation is applied to Azure functions that will be triggered by a call to the HTTP endpoint that
+ * the function is located at. The HttpTrigger annotation should be applied to a method parameter of type
+ * {@link com.microsoft.azure.serverless.functions.HttpRequestMessage}. For example:</p>
+ *
+ * <pre>
+{@literal @}FunctionName("hello")
+ public HttpResponseMessage&lt;String&gt; helloFunction(
+    {@literal @}HttpTrigger(name = "req", methods = {"get"}, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage&lt;Optional&lt;String&gt;&gt; request,
+     final ExecutionContext context) {
+     ....
+ }</pre>
+ *
+ * <p>In this code snippet you will observe that we have a function annotated with {@code @FunctionName("hello")},
+ * which indicates that this function will be available at the endpoint /api/hello. The name of the method itself, in
+ * this case {@code helloFunction} is irrelevant for all intents and purposes related to Azure Functions. Note however
+ * that the method return type is {@link com.microsoft.azure.serverless.functions.HttpResponseMessage}, and
+ * that the first argument into the function is an {@link com.microsoft.azure.serverless.functions.HttpRequestMessage}
+ * with generic type {@code Optional<String>}. This indicates that the body of the request will potentially contain
+ * a String value.</p>
+ *
+ * <p>Most important of all however is the {@code @HttpTrigger} annotation that has been applied
+ * to this argument. In this annotation you'll note that it has been given a name, as well as told what type of requests
+ * it supports (in this case, only HTTP GET requests), and that the {@link AuthorizationLevel} is anonymous, allowing
+ * access to anyone who can call the endpoint.</p>
+ *
+ * @see com.microsoft.azure.serverless.functions.HttpRequestMessage
+ * @see com.microsoft.azure.serverless.functions.HttpResponseMessage
+ * @since 1.0.0
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.PARAMETER)
 public @interface HttpTrigger {
+    /**
+     * @return The name of the HTTP trigger.
+     */
     String name();
 
     String dataType() default "";
