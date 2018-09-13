d84d7b72a6e95baf180e3c8db238cef20a17ae17
pragnagopa <pgopa@microsoft.com>
2018-09-13 15:46:46 -0700

    Update tests

--- src/test/java/com/microsoft/azure/functions/worker/functional/tests/SimpleParamReturnTests.java
+++ src/test/java/com/microsoft/azure/functions/worker/functional/tests/SimpleParamReturnTests.java
@@ -1,58 +1,61 @@
 package com.microsoft.azure.functions.worker.functional.tests;
 
-import java.util.*;
-
 import com.microsoft.azure.functions.rpc.messages.*;
-import com.microsoft.azure.functions.worker.test.categories.*;
+import com.microsoft.azure.functions.annotation.*;
 import com.microsoft.azure.functions.worker.test.utilities.*;
 import org.junit.*;
-import org.junit.experimental.categories.*;
-
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.junit.runners.Parameterized.Parameter;
 import static org.junit.Assert.*;
 
+@RunWith(Parameterized.class)
 public class SimpleParamReturnTests extends FunctionsTestBase {
-    public static String EmptyParameterFunction() {
-        return "Empty Parameter Result";
-    }
+		
+	@Parameters
+	public static Object[] data() {
+	    return new Object[] { "test String", "" };
+	}
+	
+	@Parameter 
+    public String stringInput;
 
-    private static int intReturnValue = 0;
-    public static int ReturnIntFunction() {
-        return intReturnValue;
-    }
-
-    @Test
-    @Category({IntegrationTesting.class, SmokeTesting.class, FunctionalTesting.class})
-    public void testEmptyParameter() throws Exception {
-        try (FunctionsTestHost host = new FunctionsTestHost()) {
-            this.loadFunction(host, "emptyparam", "EmptyParameterFunction");
-            InvocationResponse response = host.call("getret", "emptyparam");
+	private static int intReturnValue = 124;
+	private static String stringReturnValue;
+	
+    @FunctionName("ReturnStringTest")
+    public String ReturnStringFunction() {
+		
+		return stringReturnValue;
+	}
+	
+    @FunctionName("ReturnIntTest")
+	public static int ReturnIntFunction() {
+		return intReturnValue;
+	}
+	
+	@Test
+    public void testStringData()  throws Exception {
+		System.out.println("Run Test {}..."+ stringInput);
+		stringReturnValue = stringInput;
+		try (FunctionsTestHost host = new FunctionsTestHost()) {
+			this.loadFunction(host, "returnStringTestId", "ReturnStringFunction");
+			InvocationResponse response = host.call("getret", "returnStringTestId");			
             assertEquals(TypedData.DataCase.STRING, response.getReturnValue().getDataCase());
-            assertEquals("Empty Parameter Result", response.getReturnValue().getString());
-        }
-    }
+            String receivedString = response.getReturnValue().getString();			
+			assertEquals(stringInput, receivedString);
+		}
+	}
+    
 
-    @Test
-    @Category({IntegrationTesting.class, SmokeTesting.class, FunctionalTesting.class})
-    public void testIntReturn() throws Exception {
-        try (FunctionsTestHost host = new FunctionsTestHost()) {
-            this.loadFunction(host, "intreturn", "ReturnIntFunction");
-
-            intReturnValue = 13579;
-            InvocationResponse response = host.call("getret13579", "intreturn");
-            assertEquals(TypedData.DataCase.INT, response.getReturnValue().getDataCase());
-            assertEquals(intReturnValue, response.getReturnValue().getInt());
-
-            intReturnValue = 24680;
-            response = host.call("getret24680", "intreturn");
-            assertEquals(TypedData.DataCase.INT, response.getReturnValue().getDataCase());
-            assertEquals(intReturnValue, response.getReturnValue().getInt());
-
-            for (int i = 0; i < 100; i++) {
-                intReturnValue = new Random().nextInt();
-                response = host.call("getretloop" + intReturnValue, "intreturn");
-                assertEquals(TypedData.DataCase.INT, response.getReturnValue().getDataCase());
-                assertEquals(intReturnValue, response.getReturnValue().getInt());
-            }
-        }
-    }
+	@Test	
+	public void testIntReturn() throws Exception {
+		try (FunctionsTestHost host = new FunctionsTestHost()) {
+			this.loadFunction(host, "intreturn", "ReturnIntFunction");			
+			InvocationResponse response = host.call("getret13579", "intreturn");
+			assertEquals(TypedData.DataCase.INT, response.getReturnValue().getDataCase());
+			assertEquals(intReturnValue, response.getReturnValue().getInt());
+		}
+	}
 }
