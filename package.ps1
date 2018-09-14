36413156d0cde718821f8f17b80c7df6ef2f0cdd
pragnagopa <pgopa@microsoft.com>
2018-09-14 15:08:58 -0700

    test removing download logs

--- package.ps1
+++ package.ps1
@@ -13,7 +13,7 @@ function StopOnFailedExecution {
 Write-Host "buildNumber: " $buildNumber
 Get-Command mvn
 Get-Command nuget
-mvn --batch-mode clean package
+mvn clean package -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn -B
 StopOnFailedExecution
 remove-item pkg -Recurse -ErrorAction Ignore
 mkdir pkg
