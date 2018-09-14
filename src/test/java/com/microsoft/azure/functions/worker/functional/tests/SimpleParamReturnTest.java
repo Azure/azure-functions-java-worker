e891a13060d130fb0f909250e3e7e1c9ee013f69
pragnagopa <pgopa@microsoft.com>
2018-09-14 15:22:33 -0700

    Format

--- src/test/java/com/microsoft/azure/functions/worker/functional/tests/SimpleParamReturnTest.java
+++ src/test/java/com/microsoft/azure/functions/worker/functional/tests/SimpleParamReturnTest.java
@@ -12,29 +12,29 @@ import static org.junit.Assert.*;
 
 @RunWith(Parameterized.class)
 public class SimpleParamReturnTest extends FunctionsTestBase {
-		
+
 	@Parameters
 	public static Object[] data() {
-	    return new Object[] { "test String", "" };
+		return new Object[] { "test String", "" };
 	}
-	
-	@Parameter 
-    public String stringInput;
+
+	@Parameter
+	public String stringInput;
 	private static String stringReturnValue;
-    
-    public String ReturnStringFunction() {
-		
+
+	public String ReturnStringFunction() {
+
 		return stringReturnValue;
 	}
-	
+
 	@Test
-    public void testStringData()  throws Exception {		
+	public void testStringData() throws Exception {
 		stringReturnValue = stringInput;
 		try (FunctionsTestHost host = new FunctionsTestHost()) {
-            this.loadFunction(host, "returnStringTestId", "ReturnStringFunction");
-            InvocationResponse stringResponse = host.call("getret", "returnStringTestId");			
-            assertEquals(TypedData.DataCase.STRING, stringResponse.getReturnValue().getDataCase());            	
-            assertEquals(stringInput, stringResponse.getReturnValue().getString());
+			this.loadFunction(host, "returnStringTestId", "ReturnStringFunction");
+			InvocationResponse stringResponse = host.call("getret", "returnStringTestId");
+			assertEquals(TypedData.DataCase.STRING, stringResponse.getReturnValue().getDataCase());
+			assertEquals(stringInput, stringResponse.getReturnValue().getString());
 		}
 	}
 }
