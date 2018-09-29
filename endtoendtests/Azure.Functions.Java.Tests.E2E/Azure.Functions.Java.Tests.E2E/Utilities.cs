b7be33cee750034c63734803bbbd70427bf6c679
pragnagopa <pgopa@microsoft.com>
2018-09-29 13:40:44 -0700

    Add CosmosDB EndToEndTests

--- endtoendtests/Azure.Functions.Java.Tests.E2E/Azure.Functions.Java.Tests.E2E/Utilities.cs
+++ endtoendtests/Azure.Functions.Java.Tests.E2E/Azure.Functions.Java.Tests.E2E/Utilities.cs
@@ -1,7 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
 using System.Diagnostics;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Azure.Functions.Java.Tests.E2E
--- endtoendtests/src/main/java/com/microsoft/azure/functions/endtoend/CosmosDBTriggerTests.java
+++ endtoendtests/src/main/java/com/microsoft/azure/functions/endtoend/CosmosDBTriggerTests.java
@@ -2,74 +2,62 @@ package com.microsoft.azure.functions.endtoendtests;
 
 import com.microsoft.azure.functions.annotation.*;
 import com.microsoft.azure.functions.*;
+import com.fasterxml.jackson.annotation.*;
+import com.fasterxml.jackson.databind.*;
+
+import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.*;
 
 /**
  * Azure Functions with Azure Cosmos DB.
  */
 public class CosmosDBTriggerTests {
-     /**
-     * This function will be invoked when there are inserts or updates in the specified database and collection.
-     */
-    @FunctionName("CosmosDBTrigger")
-    public void cosmosdbTrigger(
-        @CosmosDBTrigger(
-            name = "item",
-            databaseName = "%CosmosDBDatabaseName%",
-            collectionName = "%CosmosDBCollectionName%",
-            leaseCollectionName="leaseCollection",
-            connectionStringSetting = "AzureWebJobsCosmosDBConnectionString",
-            createLeaseCollectionIfNotExists = true
-        ) Object item,
-        final ExecutionContext context
-    ) {
-        context.getLogger().info("Java Cosmos DB trigger function executed. Get a new document: " + item.toString());
-    }
 
     /**
-    * This function will be invoked when a message add to the queue. The message contents are provided as the input to this function.
-    */
+     * This function will be invoked when a message add to the queue. The message
+     * contents are provided as the input to this function.
+     */
     @FunctionName("CosmosDBInput")
     public void cosmosDBInput(
-        @QueueTrigger(name = "message", queueName = "mydbqueue", connection = "AzureWebJobsStorage") String message,
-        @CosmosDBInput(
-            name = "item",
-            databaseName = "%CosmosDBDatabaseName%",
-            collectionName = "%CosmosDBCollectionName%",
-            connectionStringSetting = "AzureWebJobsCosmosDBConnectionString",
-            id = "{queueTrigger}"
-        ) Object item,
-        final ExecutionContext context
-    ) {
+            @QueueTrigger(name = "message", queueName = "mydbqueue", connection = "AzureWebJobsStorage") String message,
+            @CosmosDBInput(name = "item", databaseName = "%CosmosDBDatabaseName%", collectionName = "%CosmosDBCollectionName%", connectionStringSetting = "AzureWebJobsCosmosDBConnectionString", id = "{queueTrigger}") Object item,
+            final ExecutionContext context) {
         context.getLogger().info("Java Queue Trigger post an id:" + message);
         context.getLogger().info("Java Cosmos DB Input function processed a document: " + item.toString());
     }
 
     /**
-    * This function will be invoked when a post request with file to http://localhost:7071/api/CosmosDBOutput. A new document will add to the collection.
-    */
-    @FunctionName("CosmosDBOutput")
-    public void cosmosDBOutOutput(
-        @HttpTrigger(name = "req", methods = {HttpMethod.GET, HttpMethod.POST}, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
-        @CosmosDBOutput(
-            name = "item",
-            databaseName = "%CosmosDBDatabaseName%",
-            collectionName = "%CosmosDBCollectionName%",
-            connectionStringSetting = "AzureWebJobsCosmosDBConnectionString"            
-        ) OutputBinding<Document> item,
-        final ExecutionContext context
-    ) {
-        String description = request.getBody().orElse("default message");
-        item.setValue(new Document(UUID.randomUUID().toString(), description)); 
-        context.getLogger().info("Java Cosmos DB Output function processed a document with description: " + description);
+     * This function will be invoked when a post request with file to
+     * http://localhost:7071/api/CosmosDBOutput. A new document will add to the
+     * collection.
+     */
+    @FunctionName("cosmosTriggerAndOutput")
+    public void cosmosTriggerAndOutput(
+            @CosmosDBTrigger(name = "itemIn", databaseName = "%CosmosDBDatabaseName%", collectionName = "ItemCollectionIn", leaseCollectionName = "leases", connectionStringSetting = "AzureWebJobsCosmosDBConnectionString", createLeaseCollectionIfNotExists = true) Object inputItem,
+            @CosmosDBOutput(name = "itemOut", databaseName = "%CosmosDBDatabaseName%", collectionName = "ItemCollectionOut", connectionStringSetting = "AzureWebJobsCosmosDBConnectionString") OutputBinding<Document> outPutItem,
+            final ExecutionContext context) {
+
+        context.getLogger().info("Java Cosmos DB trigger function executed. Received document: " + inputItem);
+
+        ArrayList inputItems = (ArrayList) inputItem;
+        String objString = inputItems.get(0).toString();
+
+        String[] arrOfStr = objString.split("=", 2);
+        String[] arrOfStrWithId = arrOfStr[1].split(",", 2);
+
+        ObjectMapper mapper = new ObjectMapper();
+        String docId = arrOfStrWithId[0];
+        context.getLogger().info("Writing to CosmosDB output binding Document id: " + docId);
+        outPutItem.setValue(new Document(docId, "testdescription"));
     }
 
     public static class Document {
-        public String ID;
-        public String Description;        
+        public String id;
+        public String Description;
 
         public Document(String id, String description) {
-            this.ID = id;
+            this.id = id;
             this.Description = description;
         }
     }
