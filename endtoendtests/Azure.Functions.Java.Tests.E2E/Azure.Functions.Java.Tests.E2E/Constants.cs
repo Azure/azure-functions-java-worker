a876d8a5c505942ae46822d175f3320b9093567d
pragnagopa <pgopa@microsoft.com>
2018-10-09 13:18:05 -0700

    Add more CosmosDBInput tests

--- endtoendtests/Azure.Functions.Java.Tests.E2E/Azure.Functions.Java.Tests.E2E/Constants.cs
+++ endtoendtests/Azure.Functions.Java.Tests.E2E/Azure.Functions.Java.Tests.E2E/Constants.cs
@@ -9,16 +9,22 @@ namespace Azure.Functions.Java.Tests.E2E
     {
         public static string FunctionsHostUrl = "http://localhost:5000";
         public static string StorageConnectionStringSetting = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
+
+        //Queue tests
         public static string OutputBindingQueueName = "test-output-java";
         public static string InputBindingQueueName = "test-input-java";
         public static string OutputBindingQueueNamePOJO = "test-output-java-pojo";
         public static string InputBindingQueueNamePOJO = "test-input-java-pojo";
         public static string TestQueueMessage = "Hello, World";
 
+        // CosmosDB tests
         public static string CosmosDBConnectionStringSetting = Environment.GetEnvironmentVariable("AzureWebJobsCosmosDBConnectionString");
         public static string DocDbDatabaseName = "ItemDb";
         public static string InputDocDbCollectionName = "ItemCollectionIn";
         public static string OutputDocDbCollectionName = "ItemCollectionOut";
         public static string DocDbLeaseCollectionName = "leases";
+        public static string InputItemsCollectionName = "ItemsCollectionIn";
+        public static string OutputItemsCollectionName = "ItemsCollectionOut";
+        public static string LeaseItemsCollectionName = "Itemsleases";
     }
 }
