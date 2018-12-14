5b730a291ec737fb474166cf763bd353f1457a45
Hamid Safi <hamids@microsoft.com>
2018-12-13 16:04:02 -0800

    Adding PicoHelper.Java

--- src/main/java/com/microsoft/azure/functions/worker/handler/FunctionEnvironmentReloadRequestHandler.java
+++ src/main/java/com/microsoft/azure/functions/worker/handler/FunctionEnvironmentReloadRequestHandler.java
@@ -4,6 +4,11 @@ import java.lang.reflect.Field;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Level;
+
+import com.sun.jna.Native;
+import com.sun.jna.platform.win32.Kernel32;
+import com.sun.jna.platform.win32.Kernel32Util;
 
 import com.microsoft.azure.functions.rpc.messages.*;
 import com.microsoft.azure.functions.rpc.messages.FunctionEnvironmentReloadResponse.Builder;
@@ -18,8 +23,10 @@ public class FunctionEnvironmentReloadRequestHandler
         FunctionEnvironmentReloadResponse.Builder::setResult,
         StreamingMessage.Builder::setFunctionEnvironmentReloadResponse);
 
-    this.broker = broker;
+    this.broker = broker;    
   }
+
+  public Map<String, String> EnvironmentVariables = new HashMap<>();
 
   @Override
   String execute(FunctionEnvironmentReloadRequest request, Builder response) throws Exception {
@@ -43,36 +50,49 @@ public class FunctionEnvironmentReloadRequestHandler
    * if java language worker is started in standby mode by the functions runtime
    * and should not be used for other purposes
    */
-  public static void setEnv(Map<String, String> newSettings) throws Exception {
+  public void setEnv(Map<String, String> newSettings) throws Exception {
     if (newSettings == null || newSettings.isEmpty()) {    
       return;
     }
-	  try {
-	    Class<?> processEnvironmentClass = Class.forName("java.lang.ProcessEnvironment");
-	    Field theEnvironmentField = processEnvironmentClass.getDeclaredField("theEnvironment");
-	    theEnvironmentField.setAccessible(true);
-	    Map<String, String> env = (Map<String, String>) theEnvironmentField.get(null);
-	    env.putAll(newSettings);
-	    Field theCaseInsensitiveEnvironmentField = processEnvironmentClass.getDeclaredField("theCaseInsensitiveEnvironment");
-	    theCaseInsensitiveEnvironmentField.setAccessible(true);
-	    Map<String, String> cienv = (Map<String, String>)     theCaseInsensitiveEnvironmentField.get(null);
-	    cienv.putAll(newSettings);
-	  } catch (NoSuchFieldException e) {
-	    Class[] classes = Collections.class.getDeclaredClasses();
-	    Map<String, String> env = System.getenv();
-	    for(Class cl : classes) {
-	      if("java.util.Collections$UnmodifiableMap".equals(cl.getName())) {
-	        Field field = cl.getDeclaredField("m");
-	        field.setAccessible(true);
-	        Object obj = field.get(env);
-	        Map<String, String> map = (Map<String, String>) obj;
-	        map.clear();
-	        map.putAll(newSettings);
-	      }
-	    }
-	  }
-	}
 
+    for (Map.Entry<String,String> entry : newSettings.entrySet()) {
+        Kernel32.INSTANCE.SetEnvironmentVariable(entry.getKey(), entry.getValue());
+      }
+
+      Kernel32.INSTANCE.SetEnvironmentVariable("WEBSITE_PLACEHOLDER_MODE", null);
+      Kernel32.INSTANCE.SetEnvironmentVariable("WEBSITE_PLACEHOLDER_MODE", "0");
+
+      PicoHelper picoHelperInstance = PicoHelper.INSTANCE;
+
+      picoHelperInstance.ReinitializeDetours();
+
+      try {
+        Class<?> processEnvironmentClass = Class.forName("java.lang.ProcessEnvironment");
+        Field theEnvironmentField = processEnvironmentClass.getDeclaredField("theEnvironment");
+        theEnvironmentField.setAccessible(true);
+        Map<String, String> env = (Map<String, String>) theEnvironmentField.get(null);
+        env.clear();
+        env.putAll(newSettings);
+        Field theCaseInsensitiveEnvironmentField = processEnvironmentClass.getDeclaredField("theCaseInsensitiveEnvironment");
+        theCaseInsensitiveEnvironmentField.setAccessible(true);
+        Map<String, String> cienv = (Map<String, String>)     theCaseInsensitiveEnvironmentField.get(null);
+        cienv.clear();
+        cienv.putAll(newSettings);
+      } catch (NoSuchFieldException e) {
+        Class[] classes = Collections.class.getDeclaredClasses();
+        Map<String, String> env = System.getenv();
+        for(Class cl : classes) {
+          if("java.util.Collections$UnmodifiableMap".equals(cl.getName())) {
+            Field field = cl.getDeclaredField("m");
+            field.setAccessible(true);
+            Object obj = field.get(env);
+            Map<String, String> map = (Map<String, String>) obj;
+            map.clear();
+            map.putAll(newSettings);
+          }
+        }
+      }
+    }
+	
   private final JavaFunctionBroker broker;
-  public static Map<String, String> EnvironmentVariables = new HashMap<>();
 }
