9e0ec44511f7489f9831fd3272426bd2c25407d0
pragnagopa <pgopa@microsoft.com>
2019-01-09 13:03:03 -0800

    just kernel

--- src/main/java/com/microsoft/azure/functions/worker/handler/FunctionEnvironmentReloadRequestHandler.java
+++ src/main/java/com/microsoft/azure/functions/worker/handler/FunctionEnvironmentReloadRequestHandler.java
@@ -15,15 +15,15 @@ import com.microsoft.azure.functions.rpc.messages.FunctionEnvironmentReloadRespo
 import com.microsoft.azure.functions.worker.broker.JavaFunctionBroker;
 import com.microsoft.azure.functions.worker.description.FunctionMethodDescriptor;
 
-public class FunctionEnvironmentReloadRequestHandler
-    extends MessageHandler<FunctionEnvironmentReloadRequest, FunctionEnvironmentReloadResponse.Builder> {
+public class FunctionEnvironmentReloadRequestHandler extends
+    MessageHandler<FunctionEnvironmentReloadRequest, FunctionEnvironmentReloadResponse.Builder> {
   public FunctionEnvironmentReloadRequestHandler(JavaFunctionBroker broker) {
     super(StreamingMessage::getFunctionEnvironmentReloadRequest,
         FunctionEnvironmentReloadResponse::newBuilder,
         FunctionEnvironmentReloadResponse.Builder::setResult,
         StreamingMessage.Builder::setFunctionEnvironmentReloadResponse);
 
-    this.broker = broker;    
+    this.broker = broker;
   }
 
   public Map<String, String> EnvironmentVariables = new HashMap<>();
@@ -31,11 +31,12 @@ public class FunctionEnvironmentReloadRequestHandler
   @Override
   String execute(FunctionEnvironmentReloadRequest request, Builder response) throws Exception {
     EnvironmentVariables = request.getEnvironmentVariablesMap();
-    if (EnvironmentVariables == null || EnvironmentVariables.isEmpty()) {    
-      return String.format("Ignoring FunctionEnvironmentReloadRequest as newSettings map is either empty or null");
+    if (EnvironmentVariables == null || EnvironmentVariables.isEmpty()) {
+      return String.format(
+          "Ignoring FunctionEnvironmentReloadRequest as newSettings map is either empty or null");
     }
-    
-    specialize(EnvironmentVariables);    
+
+    setEnv(EnvironmentVariables);
     return String.format("FunctionEnvironmentReloadRequest completed");
   }
 
@@ -44,55 +45,26 @@ public class FunctionEnvironmentReloadRequestHandler
     return new FunctionMethodDescriptor(functionId, metadata.getName(), metadata.getEntryPoint(),
         metadata.getScriptFile());
   }
-  
+
   /*
-   * This is a helper utility specifically to reload environment variables and
-   * ReinitializeDetours if java language worker is started in standby mode by the functions runtime
-   * and should not be used for other purposes
+   * This is a helper utility specifically to reload environment variables if java language worker
+   * is started in standby mode by the functions runtime and should not be used for other purposes
    */
-  public void specialize(Map<String, String> newSettings) throws Exception {
-    if (newSettings == null || newSettings.isEmpty()) {    
+  public void setEnv(Map<String, String> newSettings) throws Exception {
+    if (newSettings == null || newSettings.isEmpty()) {
       return;
     }
 
-    // This is required to reinitialize file detours
-    newSettings.put(WebsitePlaceholderMode, "0");
-    try {
-        Class<?> processEnvironmentClass = Class.forName("java.lang.ProcessEnvironment");
-        Field theEnvironmentField = processEnvironmentClass.getDeclaredField("theEnvironment");
-        theEnvironmentField.setAccessible(true);
-        Map<String, String> env = (Map<String, String>) theEnvironmentField.get(null);
-        env.clear();
-        env.putAll(newSettings);
-        Field theCaseInsensitiveEnvironmentField = processEnvironmentClass.getDeclaredField("theCaseInsensitiveEnvironment");
-        theCaseInsensitiveEnvironmentField.setAccessible(true);
-        Map<String, String> cienv = (Map<String, String>)     theCaseInsensitiveEnvironmentField.get(null);
-        cienv.clear();
-        cienv.putAll(newSettings);
-      } catch (NoSuchFieldException e) {
-        Class[] classes = Collections.class.getDeclaredClasses();
-        Map<String, String> env = System.getenv();
-        for(Class cl : classes) {
-          if("java.util.Collections$UnmodifiableMap".equals(cl.getName())) {
-            Field field = cl.getDeclaredField("m");
-            field.setAccessible(true);
-            Object obj = field.get(env);
-            Map<String, String> map = (Map<String, String>) obj;
-            map.clear();
-            map.putAll(newSettings);
-          }
-        }
-      }
-      
-      String  azureWebsiteInstanceId = System.getenv(AzureWebsiteInstanceId);
-      if (azureWebsiteInstanceId != null && azureWebsiteInstanceId != "")
-      {
-    	  PicoHelper picoHelperInstance = PicoHelper.INSTANCE;
-          picoHelperInstance.ReinitializeDetours();
-      }      
+    for (Map.Entry<String, String> entry : newSettings.entrySet()) {
+      Kernel32.INSTANCE.SetEnvironmentVariable(entry.getKey(), entry.getValue());
     }
-	
+
+    Kernel32.INSTANCE.SetEnvironmentVariable("WEBSITE_PLACEHOLDER_MODE", null);
+    Kernel32.INSTANCE.SetEnvironmentVariable("WEBSITE_PLACEHOLDER_MODE", "0");
+
+    PicoHelper picoHelperInstance = PicoHelper.INSTANCE;
+    picoHelperInstance.ReinitializeDetours();
+  }
+
   private final JavaFunctionBroker broker;
-  public final String AzureWebsiteInstanceId = "WEBSITE_INSTANCE_ID";
-  public final String WebsitePlaceholderMode = "WEBSITE_PLACEHOLDER_MODE";
 }
--- src/main/java/com/microsoft/azure/functions/worker/reflect/DefaultClassLoaderProvider.java
+++ src/main/java/com/microsoft/azure/functions/worker/reflect/DefaultClassLoaderProvider.java
@@ -14,78 +14,75 @@ import com.microsoft.azure.functions.worker.*;
  * @author Kevin Hillinger Default implementation of the class loader provider
  */
 public class DefaultClassLoaderProvider implements ClassLoaderProvider {
-	public DefaultClassLoaderProvider() {
-		urls = Collections.newSetFromMap(new ConcurrentHashMap<URL, Boolean>());
-	}
+  public DefaultClassLoaderProvider() {
+    urls = Collections.newSetFromMap(new ConcurrentHashMap<URL, Boolean>());
+  }
 
-	/*
-	 * @see
-	 * com.microsoft.azure.functions.reflect.ClassLoaderProvider#getClassLoader()
-	 */
-	@Override
-	public ClassLoader getClassLoader() {
-		URL[] urlsForClassLoader = new URL[urls.size()];
-		urls.toArray(urlsForClassLoader);
+  /*
+   * @see com.microsoft.azure.functions.reflect.ClassLoaderProvider#getClassLoader()
+   */
+  @Override
+  public ClassLoader getClassLoader() {
+    URL[] urlsForClassLoader = new URL[urls.size()];
+    urls.toArray(urlsForClassLoader);
 
-		URLClassLoader classLoader = new URLClassLoader(urlsForClassLoader);
-		Thread.currentThread().setContextClassLoader(classLoader);
+    URLClassLoader classLoader = new URLClassLoader(urlsForClassLoader);
+    Thread.currentThread().setContextClassLoader(classLoader);
 
-		return classLoader;
-	}
+    return classLoader;
+  }
 
-	@Override
-	public void addDirectory(File directory) throws MalformedURLException, IOException {
-		if (!directory.exists()) {
-			return;
-		}
+  @Override
+  public void addDirectory(File directory) throws MalformedURLException, IOException {
+    if (!directory.exists()) {
+      return;
+    }
 
-		File[] jarFiles = directory.listFiles(new FileFilter() {
-			@Override
-			public boolean accept(File file) {
-				return file.isFile() && file.getName().endsWith(".jar");
-			}
-		});
+    File[] jarFiles = directory.listFiles(new FileFilter() {
+      @Override
+      public boolean accept(File file) {
+        return file.isFile() && file.getName().endsWith(".jar");
+      }
+    });
 
-		for (File file : jarFiles) {
-			addUrl(file.toURI().toURL());
-		}
-	}
+    for (File file : jarFiles) {
+      addUrl(file.toURI().toURL());
+    }
+  }
 
-	@Override
-	public void addUrl(URL url) throws IOException {
-		if (urls.contains(url)) {
-			return;
-		}
+  @Override
+  public void addUrl(URL url) throws IOException {
+    if (urls.contains(url)) {
+      return;
+    }
+    
+    WorkerLogManager.getSystemLogger().info("Loading file URL: " + url);    
 
-		if (!isUrlPointingToAFile(url)) {
-			throw new IOException("The jar URL \"" + url + "\" being added does not exist.");
-		}
+    urls.add(url);
+    addUrlToSystemClassLoader(url);
+  }
 
-		urls.add(url);
-		addUrlToSystemClassLoader(url);
-	}
+  public static boolean isUrlPointingToAFile(URL url) throws UnsupportedEncodingException {
+    String decodedPath = URLDecoder.decode(url.getPath(), "UTF-8");
+    File file = new File(decodedPath);
+    return file.exists();
+  }
 
-	public static boolean isUrlPointingToAFile(URL url) throws UnsupportedEncodingException {
-		String decodedPath = URLDecoder.decode(url.getPath(), "UTF-8");
-		File file = new File(decodedPath);
-		return file.exists();
-	}
+  private void addUrlToSystemClassLoader(URL url) throws IOException {
+    URLClassLoader sysloader = (URLClassLoader) ClassLoader.getSystemClassLoader();
+    Class<?> sysclass = URLClassLoader.class;
 
-	private void addUrlToSystemClassLoader(URL url) throws IOException {
-		URLClassLoader sysloader = (URLClassLoader) ClassLoader.getSystemClassLoader();
-		Class<?> sysclass = URLClassLoader.class;
+    try {
+      Method method = sysclass.getDeclaredMethod(SYS_LOADER_ADDURL_METHOD_NAME, parameters);
+      method.setAccessible(true);
+      method.invoke(sysloader, new Object[] { url });
+    } catch (Throwable t) {
+      throw new IOException("Error adding " + url + " to system classloader");
+    }
 
-		try {
-			Method method = sysclass.getDeclaredMethod(SYS_LOADER_ADDURL_METHOD_NAME, parameters);
-			method.setAccessible(true);
-			method.invoke(sysloader, new Object[] { url });
-		} catch (Throwable t) {
-			throw new IOException("Error adding " + url + " to system classloader");
-		}
+  }
 
-	}
-
-	private static final String SYS_LOADER_ADDURL_METHOD_NAME = "addURL";
-	private static final Class<?>[] parameters = new Class[] { URL.class };
-	private final Set<URL> urls;
+  private static final String SYS_LOADER_ADDURL_METHOD_NAME = "addURL";
+  private static final Class<?>[] parameters = new Class[] { URL.class };
+  private final Set<URL> urls;
 }
