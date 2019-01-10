5a0745a6e40f73eef23b6cc2962b002bec8a4ca9
Henry Hamid Safi <hamids@microsoft.com>
2019-01-10 15:08:17 -0800

    Adding PicoHelper.Java (#253)
    * Update environment variables at the process and JVM level
    * Call PicoHelper to reinitialize file detours

--- src/main/java/com/microsoft/azure/functions/worker/handler/FunctionEnvironmentReloadRequestHandler.java
+++ src/main/java/com/microsoft/azure/functions/worker/handler/FunctionEnvironmentReloadRequestHandler.java
@@ -5,13 +5,14 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.sun.jna.platform.win32.Kernel32;
+
 import com.microsoft.azure.functions.rpc.messages.*;
 import com.microsoft.azure.functions.rpc.messages.FunctionEnvironmentReloadResponse.Builder;
 import com.microsoft.azure.functions.worker.broker.JavaFunctionBroker;
-import com.microsoft.azure.functions.worker.description.FunctionMethodDescriptor;
 
-public class FunctionEnvironmentReloadRequestHandler
-    extends MessageHandler<FunctionEnvironmentReloadRequest, FunctionEnvironmentReloadResponse.Builder> {
+public class FunctionEnvironmentReloadRequestHandler extends
+    MessageHandler<FunctionEnvironmentReloadRequest, FunctionEnvironmentReloadResponse.Builder> {
   public FunctionEnvironmentReloadRequestHandler(JavaFunctionBroker broker) {
     super(StreamingMessage::getFunctionEnvironmentReloadRequest,
         FunctionEnvironmentReloadResponse::newBuilder,
@@ -21,58 +22,78 @@ public class FunctionEnvironmentReloadRequestHandler
     this.broker = broker;
   }
 
+  public Map<String, String> EnvironmentVariables = new HashMap<>();
+
   @Override
   String execute(FunctionEnvironmentReloadRequest request, Builder response) throws Exception {
     EnvironmentVariables = request.getEnvironmentVariablesMap();
-    if (EnvironmentVariables == null || EnvironmentVariables.isEmpty()) {    
-      return String.format("Ignoring FunctionEnvironmentReloadRequest as newSettings map is either empty or null");
+    if (EnvironmentVariables == null || EnvironmentVariables.isEmpty()) {
+      return String.format(
+          "Ignoring FunctionEnvironmentReloadRequest as newSettings map is either empty or null");
     }
-    
-    setEnv(EnvironmentVariables);    
+    setEnv(EnvironmentVariables);
     return String.format("FunctionEnvironmentReloadRequest completed");
   }
 
-  FunctionMethodDescriptor createFunctionDescriptor(String functionId,
-      RpcFunctionMetadata metadata) {
-    return new FunctionMethodDescriptor(functionId, metadata.getName(), metadata.getEntryPoint(),
-        metadata.getScriptFile());
-  }
-  
   /*
-   * This is a helper utility specifically to reload environment variables 
-   * if java language worker is started in standby mode by the functions runtime
-   * and should not be used for other purposes
+   * This is a helper utility specifically to reload environment variables if java language worker
+   * is started in standby mode by the functions runtime and should not be used for other purposes
    */
-  public static void setEnv(Map<String, String> newSettings) throws Exception {
-    if (newSettings == null || newSettings.isEmpty()) {    
+  public void setEnv(Map<String, String> newSettings) throws Exception {
+    if (newSettings == null || newSettings.isEmpty()) {
       return;
     }
-	  try {
-	    Class<?> processEnvironmentClass = Class.forName("java.lang.ProcessEnvironment");
-	    Field theEnvironmentField = processEnvironmentClass.getDeclaredField("theEnvironment");
-	    theEnvironmentField.setAccessible(true);
-	    Map<String, String> env = (Map<String, String>) theEnvironmentField.get(null);
-	    env.putAll(newSettings);
-	    Field theCaseInsensitiveEnvironmentField = processEnvironmentClass.getDeclaredField("theCaseInsensitiveEnvironment");
-	    theCaseInsensitiveEnvironmentField.setAccessible(true);
-	    Map<String, String> cienv = (Map<String, String>)     theCaseInsensitiveEnvironmentField.get(null);
-	    cienv.putAll(newSettings);
-	  } catch (NoSuchFieldException e) {
-	    Class[] classes = Collections.class.getDeclaredClasses();
-	    Map<String, String> env = System.getenv();
-	    for(Class cl : classes) {
-	      if("java.util.Collections$UnmodifiableMap".equals(cl.getName())) {
-	        Field field = cl.getDeclaredField("m");
-	        field.setAccessible(true);
-	        Object obj = field.get(env);
-	        Map<String, String> map = (Map<String, String>) obj;
-	        map.clear();
-	        map.putAll(newSettings);
-	      }
-	    }
-	  }
-	}
+
+    // Update Environment variables at the process level
+    Map<String, String> oldSettings = System.getenv();
+    for (Map.Entry<String, String> oldEntry : oldSettings.entrySet()) {
+      Kernel32.INSTANCE.SetEnvironmentVariable(oldEntry.getKey(), null);
+    }
+
+    for (Map.Entry<String, String> entry : newSettings.entrySet()) {
+      Kernel32.INSTANCE.SetEnvironmentVariable(entry.getKey(), entry.getValue());
+    }   
+
+    Kernel32.INSTANCE.SetEnvironmentVariable(WebsitePlaceholderMode, null);
+
+    String azureWebsiteInstanceId = System.getenv(AzureWebsiteInstanceId);
+    if (azureWebsiteInstanceId != null && !azureWebsiteInstanceId.isEmpty()) {
+      PicoHelper picoHelperInstance = PicoHelper.INSTANCE;
+      picoHelperInstance.ReinitializeDetours();
+    }
+
+    // Update Environment variables in the JVM
+    try {
+      Class<?> processEnvironmentClass = Class.forName("java.lang.ProcessEnvironment");
+      Field theEnvironmentField = processEnvironmentClass.getDeclaredField("theEnvironment");
+      theEnvironmentField.setAccessible(true);
+      Map<String, String> env = (Map<String, String>) theEnvironmentField.get(null);
+      env.clear();
+      env.putAll(newSettings);
+      Field theCaseInsensitiveEnvironmentField = processEnvironmentClass
+          .getDeclaredField("theCaseInsensitiveEnvironment");
+      theCaseInsensitiveEnvironmentField.setAccessible(true);
+      Map<String, String> cienv = (Map<String, String>) theCaseInsensitiveEnvironmentField
+          .get(null);
+      cienv.clear();
+      cienv.putAll(newSettings);
+    } catch (NoSuchFieldException e) {
+      Class[] classes = Collections.class.getDeclaredClasses();
+      Map<String, String> env = System.getenv();
+      for (Class cl : classes) {
+        if ("java.util.Collections$UnmodifiableMap".equals(cl.getName())) {
+          Field field = cl.getDeclaredField("m");
+          field.setAccessible(true);
+          Object obj = field.get(env);
+          Map<String, String> map = (Map<String, String>) obj;
+          map.clear();
+          map.putAll(newSettings);
+        }
+      }
+    }
+  }
 
   private final JavaFunctionBroker broker;
-  public static Map<String, String> EnvironmentVariables = new HashMap<>();
+  public final String WebsitePlaceholderMode = "WEBSITE_PLACEHOLDER_MODE";
+  public final String AzureWebsiteInstanceId = "WEBSITE_INSTANCE_ID";
 }
