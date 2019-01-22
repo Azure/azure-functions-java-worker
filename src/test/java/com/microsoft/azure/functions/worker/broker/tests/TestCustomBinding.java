1c3a19bdbfe148e9c26615f942cef8682bc005c0
pragnagopa <pgopa@microsoft.com>
2019-01-22 13:54:47 -0800

    Add support for CustomBinding

---
+++ src/test/java/com/microsoft/azure/functions/worker/broker/tests/TestCustomBinding.java
@@ -0,0 +1,16 @@
+package com.microsoft.azure.functions.worker.broker.tests;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.ElementType;
+
+import com.microsoft.azure.functions.annotation.CustomBinding;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+@CustomBinding(direction = "in", name = "message", type = "customBinding")
+public @interface TestCustomBinding {
+   String index();
+   String path();
+}
