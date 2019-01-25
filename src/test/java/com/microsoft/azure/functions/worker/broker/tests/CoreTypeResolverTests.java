9e119e6ae54c9cf421c406d28d84714cce0a37e7
Pragna Gopa <pgopa@microsoft.com>
2019-01-25 09:53:17 -0800

    Add support for CustomBinding (#263)

--- src/test/java/com/microsoft/azure/functions/worker/broker/tests/CoreTypeResolverTests.java
+++ src/test/java/com/microsoft/azure/functions/worker/broker/tests/CoreTypeResolverTests.java
@@ -1,9 +1,14 @@
 package com.microsoft.azure.functions.worker.broker.tests;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.*;
 import java.util.*;
 
-import com.microsoft.azure.functions.OutputBinding;
+import com.microsoft.azure.functions.*;
+import com.microsoft.azure.functions.annotation.*;
+import com.microsoft.azure.functions.worker.binding.tests.RpcHttpRequestDataSourceTests;
+import com.microsoft.azure.functions.worker.broker.CoreTypeResolver;
+
 import org.junit.*;
 
 import static com.microsoft.azure.functions.worker.broker.CoreTypeResolver.getRuntimeClass;
@@ -11,57 +16,135 @@ import static com.microsoft.azure.functions.worker.broker.CoreTypeResolver.isVal
 import static org.junit.Assert.*;
 
 public class CoreTypeResolverTests {
-    @Test    
-    public void testIsValidOutputType() throws Exception {
-        assertFalse(isValidOutputType(Integer.class));
-        assertFalse(isValidOutputType(int.class));
-        assertTrue(isValidOutputType(returnTypeOf("outputInteger")));
-        assertFalse(isValidOutputType(returnTypeOf("outputOutputInteger")));
-        assertTrue(isValidOutputType(ExtendedOutputBinding.class));
-        assertFalse(isValidOutputType(returnTypeOf("listInteger")));
-        assertTrue(isValidOutputType(returnTypeOf("outputListInteger")));
-        assertTrue(isValidOutputType(returnTypeOf("exoutputInteger")));
-        assertTrue(isValidOutputType(returnTypeOf("exoutputListInteger")));
-        assertFalse(isValidOutputType(returnTypeOf("exoutputOutputInteger")));
-        assertFalse(isValidOutputType(returnTypeOf("outputExoutputInteger")));
+
+  public void CustomBinding_Valid(@HttpTrigger(name = "req", methods = { HttpMethod.GET,
+      HttpMethod.POST }, authLevel = AuthorizationLevel.FUNCTION) HttpRequestMessage<String> request,
+      @TestCustomBinding(index = "testIndex", path = "testPath") String customInput) {
+  }
+
+  public void CustomBinding_Invalid(
+      @InvalidCustomBinding(index = "testIndex", path = "testPath") String customInput) {
+  }
+
+  @Test
+  public void testIsValidOutputType() throws Exception {
+    assertFalse(isValidOutputType(Integer.class));
+    assertFalse(isValidOutputType(int.class));
+    assertTrue(isValidOutputType(returnTypeOf("outputInteger")));
+    assertFalse(isValidOutputType(returnTypeOf("outputOutputInteger")));
+    assertTrue(isValidOutputType(ExtendedOutputBinding.class));
+    assertFalse(isValidOutputType(returnTypeOf("listInteger")));
+    assertTrue(isValidOutputType(returnTypeOf("outputListInteger")));
+    assertTrue(isValidOutputType(returnTypeOf("exoutputInteger")));
+    assertTrue(isValidOutputType(returnTypeOf("exoutputListInteger")));
+    assertFalse(isValidOutputType(returnTypeOf("exoutputOutputInteger")));
+    assertFalse(isValidOutputType(returnTypeOf("outputExoutputInteger")));
+  }
+
+  @Test
+  public void testGetRuntimeClass() throws Exception {
+    assertEquals(Integer.class, getRuntimeClass(Integer.class));
+    assertEquals(Integer.TYPE, getRuntimeClass(int.class));
+    assertEquals(OutputBinding.class, getRuntimeClass(returnTypeOf("outputInteger")));
+    assertEquals(OutputBinding.class, getRuntimeClass(returnTypeOf("outputOutputInteger")));
+    assertEquals(List.class, getRuntimeClass(returnTypeOf("listInteger")));
+    assertEquals(OutputBinding.class, getRuntimeClass(returnTypeOf("outputListInteger")));
+    assertEquals(GenericExtendedOutputBinding.class,
+        getRuntimeClass(returnTypeOf("exoutputOutputInteger")));
+    assertEquals(OutputBinding.class, getRuntimeClass(returnTypeOf("outputExoutputInteger")));
+  }
+
+  @Test
+  public void getNameFromCustomBinding() {
+    Method customBindingValid = getFunctionMethod("CustomBinding_Valid");
+    Parameter[] parameters = customBindingValid.getParameters();
+    for (Parameter parameter : parameters) {
+      String annotationName = CoreTypeResolver.getAnnotationName(parameter);
+      assertNotNull(annotationName);
+    }
+  }
+
+  @Test
+  public void getNameFromCustomBinding_ReturnsNull() {
+    Method customBindingInvalid = getFunctionMethod("CustomBinding_Invalid");
+    Parameter[] parameters = customBindingInvalid.getParameters();
+    for (Parameter parameter : parameters) {
+      String annotationName = CoreTypeResolver.getAnnotationName(parameter);
+      assertNull(annotationName);
+    }
+  }
+
+  private OutputBinding<Integer> outputInteger() {
+    return null;
+  }
+
+  private OutputBinding<OutputBinding<Integer>> outputOutputInteger() {
+    return null;
+  }
+
+  private List<Integer> listInteger() {
+    return null;
+  }
+
+  private OutputBinding<List<Integer>> outputListInteger() {
+    return null;
+  }
+
+  private GenericExtendedOutputBinding<Integer> exoutputInteger() {
+    return null;
+  }
+
+  private GenericExtendedOutputBinding<List<Integer>> exoutputListInteger() {
+    return null;
+  }
+
+  private GenericExtendedOutputBinding<OutputBinding<Integer>> exoutputOutputInteger() {
+    return null;
+  }
+
+  private OutputBinding<GenericExtendedOutputBinding<Integer>> outputExoutputInteger() {
+    return null;
+  }
+
+  private Type returnTypeOf(String type) throws NoSuchMethodException {
+    Method m = CoreTypeResolverTests.class.getDeclaredMethod(type);
+    m.setAccessible(true);
+    return m.getGenericReturnType();
+  }
+
+  private class ExtendedOutputBinding implements OutputBinding<Integer> {
+    @Override
+    public Integer getValue() {
+      return null;
     }
 
-    @Test    
-    public void testGetRuntimeClass() throws Exception {
-        assertEquals(Integer.class, getRuntimeClass(Integer.class));
-        assertEquals(Integer.TYPE, getRuntimeClass(int.class));
-        assertEquals(OutputBinding.class, getRuntimeClass(returnTypeOf("outputInteger")));
-        assertEquals(OutputBinding.class, getRuntimeClass(returnTypeOf("outputOutputInteger")));
-        assertEquals(List.class, getRuntimeClass(returnTypeOf("listInteger")));
-        assertEquals(OutputBinding.class, getRuntimeClass(returnTypeOf("outputListInteger")));
-        assertEquals(GenericExtendedOutputBinding.class, getRuntimeClass(returnTypeOf("exoutputOutputInteger")));
-        assertEquals(OutputBinding.class, getRuntimeClass(returnTypeOf("outputExoutputInteger")));
+    @Override
+    public void setValue(Integer value) {
+    }
+  }
+
+  private class GenericExtendedOutputBinding<T> implements OutputBinding<T> {
+    @Override
+    public T getValue() {
+      return null;
     }
 
-    private OutputBinding<Integer> outputInteger() { return null; }
-    private OutputBinding<OutputBinding<Integer>> outputOutputInteger() { return null; }
-    private List<Integer> listInteger() { return null; }
-    private OutputBinding<List<Integer>> outputListInteger() { return null; }
-    private GenericExtendedOutputBinding<Integer> exoutputInteger() { return null; }
-    private GenericExtendedOutputBinding<List<Integer>> exoutputListInteger() { return null; }
-    private GenericExtendedOutputBinding<OutputBinding<Integer>> exoutputOutputInteger() { return null; }
-    private OutputBinding<GenericExtendedOutputBinding<Integer>> outputExoutputInteger() { return null; }
-    private Type returnTypeOf(String type) throws NoSuchMethodException {
-        Method m = CoreTypeResolverTests.class.getDeclaredMethod(type);
-        m.setAccessible(true);
-        return m.getGenericReturnType();
+    @Override
+    public void setValue(T value) {
     }
+  }
 
-    private class ExtendedOutputBinding implements OutputBinding<Integer> {
-        @Override
-        public Integer getValue() { return null; }
-        @Override
-        public void setValue(Integer value) {}
+  private Method getFunctionMethod(String methodName) {
+    CoreTypeResolverTests coreTypeResolverTests = new CoreTypeResolverTests();
+    Class<? extends CoreTypeResolverTests> testsClass = coreTypeResolverTests.getClass();
+    Method[] methods = testsClass.getMethods();
+    Method functionMethod = null;
+    for (Method method : methods) {
+      if (method.getName() == methodName) {
+        functionMethod = method;
+        break;
+      }
     }
-    private class GenericExtendedOutputBinding<T> implements OutputBinding<T> {
-        @Override
-        public T getValue() { return null; }
-        @Override
-        public void setValue(T value) {}
-    }
+    return functionMethod;
+  }
 }
---
+++ src/test/java/com/microsoft/azure/functions/worker/broker/tests/InvalidCustomBinding.java
@@ -0,0 +1,13 @@
+package com.microsoft.azure.functions.worker.broker.tests;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.ElementType;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface InvalidCustomBinding {
+  String index();
+  String path();
+}
