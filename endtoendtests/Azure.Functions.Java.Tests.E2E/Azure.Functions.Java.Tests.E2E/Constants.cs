d87a829af4a9c3d6cbdd4c998f292e90bf8a91b4
Ahmed El Sayed (Mamoun) <52262708+amamounelsayed@users.noreply.github.com>
2019-07-24 18:33:53 -0700

    Add settings to constants

--- endtoendtests/Azure.Functions.Java.Tests.E2E/Azure.Functions.Java.Tests.E2E/Constants.cs
+++ endtoendtests/Azure.Functions.Java.Tests.E2E/Azure.Functions.Java.Tests.E2E/Constants.cs
@@ -53,6 +53,7 @@ namespace Azure.Functions.Java.Tests.E2E
         public static string OutputBinaryArrayManyQueueName = "test-binary-output-cardinality-many-array-java";
         public static string InputBinaryManyArrayEventHubQueueName = "test-binary-input-cardinality-many-array-java";
 
+        // Settings
         public static string EventHubsConnectionStringSenderSetting = Environment.GetEnvironmentVariable("AzureWebJobsEventHubSender");
         public static string EventHubsConnectionStringSenderSetting2 = Environment.GetEnvironmentVariable("AzureWebJobsEventHubSender_2");
 
