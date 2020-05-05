eb1d7175cae12a76986f6654a36d181aa4c9ebc9
amamounelsayed <52262708+amamounelsayed@users.noreply.github.com>
2020-05-05 16:51:20 -0700

    Revert "parent a7fb765b2427237041af691c6d2be38c54b1e60b author Tsuyoshi Ushio <ushio@simplearchitect.com> 1588194461 -0700 committer Tsuyoshi Ushio <ushio@simplearchitect.com> 1588712073 -0700"
    This reverts commit 7b00ece1785f911606913f09ea988c7801182b8f.

--- src/main/java/com/microsoft/azure/functions/worker/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.microsoft.azure.functions.worker;
-
-/**
- * The Constants file for Java language worker
- */
-public final class Constants {
-    private Constants(){}
-    public final static String TRIGGER_METADATA_DOLLAR_REQUEST_KEY = "$request";
-}
