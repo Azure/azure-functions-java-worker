06bf97d6c47942a27ed49c3d87270a2d8a0e1adb
amamounelsayed <52262708+amamounelsayed@users.noreply.github.com>
2020-05-05 16:51:20 -0700

    Revert "parent a7fb765b2427237041af691c6d2be38c54b1e60b author Tsuyoshi Ushio <ushio@simplearchitect.com> 1588194461 -0700 committer Tsuyoshi Ushio <ushio@simplearchitect.com> 1588712073 -0700"
    This reverts commit 7b00ece1785f911606913f09ea988c7801182b8f.

--- src/main/java/com/microsoft/azure/functions/worker/broker/JavaFunctionBroker.java
+++ src/main/java/com/microsoft/azure/functions/worker/broker/JavaFunctionBroker.java
@@ -3,11 +3,12 @@ package com.microsoft.azure.functions.worker.broker;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
-import java.util.*;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
 import com.microsoft.azure.functions.rpc.messages.*;
-import com.microsoft.azure.functions.worker.Constants;
 import com.microsoft.azure.functions.worker.binding.BindingDataStore;
 import com.microsoft.azure.functions.worker.description.FunctionMethodDescriptor;
 import com.microsoft.azure.functions.worker.reflect.ClassLoaderProvider;
@@ -45,7 +46,7 @@ public class JavaFunctionBroker {
 
 		BindingDataStore dataStore = new BindingDataStore();
 		dataStore.setBindingDefinitions(executor.getBindingDefinitions());
-		dataStore.addTriggerMetadataSource(getTriggerMetadataMap(request));
+		dataStore.addTriggerMetadataSource(request.getTriggerMetadataMap());
 		dataStore.addParameterSources(request.getInputDataList());
 		dataStore.addExecutionContextSource(request.getInvocationId(), methodEntry.left, request.getTraceContext().getTraceParent(), request.getTraceContext().getTraceState(), request.getTraceContext().getAttributesMap());
 
@@ -56,29 +57,6 @@ public class JavaFunctionBroker {
 
 	public Optional<String> getMethodName(String id) {
 		return Optional.ofNullable(this.methods.get(id)).map(entry -> entry.left);
-	}
-
-	// TODO the scope should be package private for testability. Modify the package name as same as main package
-	public Map<String, TypedData> getTriggerMetadataMap(InvocationRequest request) {
-		String    name ="";
-		TypedData dataWithHttp = null;
-		for(ParameterBinding e: request.getInputDataList()) {
-			if (e.getData().hasHttp()) {
-				dataWithHttp = e.getData();
-				name = e.getName();
-				break;
-			}
-		}
-
-		Map<String, TypedData> triggerMetadata = new HashMap(request.getTriggerMetadataMap());
-		if (!name.isEmpty() && !triggerMetadata.containsKey(name)) {
-			triggerMetadata.put(name, dataWithHttp);
-		}
-		String requestKey = Constants.TRIGGER_METADATA_DOLLAR_REQUEST_KEY;
-		if (dataWithHttp != null & !triggerMetadata.containsKey(requestKey)) {
-			triggerMetadata.put(requestKey, dataWithHttp);
-		}
-		return Collections.unmodifiableMap(triggerMetadata);
 	}
 
 	private void addSearchPathsToClassLoader(FunctionMethodDescriptor function) throws IOException {
