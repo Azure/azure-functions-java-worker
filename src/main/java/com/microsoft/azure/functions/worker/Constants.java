5d1a654b0f7cfdf127c094fa5daa01def745cbf1
amamounelsayed <52262708+amamounelsayed@users.noreply.github.com>
2020-05-06 10:59:28 -0700

    Support RpcHttpBodyOnly and RpcHttpTriggerMetadataRemoved

---
+++ src/main/java/com/microsoft/azure/functions/worker/Constants.java
@@ -0,0 +1,9 @@
+package com.microsoft.azure.functions.worker;
+
+/**
+ * The Constants file for Java language worker
+ */
+public final class Constants {
+    private Constants(){}
+    public final static String TRIGGER_METADATA_DOLLAR_REQUEST_KEY = "$request";
+}
