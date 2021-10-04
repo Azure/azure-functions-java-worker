d3f5674fafe9b23a2b9ca282cc9574a3ab507a10
kaibocai <89094811+kaibocai@users.noreply.github.com>
2021-10-04 11:27:49 -0500

    fix method name issue

--- endtoendtests/src/main/java/com/microsoft/azure/functions/endtoend/HttpTriggerTests.java
+++ endtoendtests/src/main/java/com/microsoft/azure/functions/endtoend/HttpTriggerTests.java
@@ -265,4 +265,40 @@ public class HttpTriggerTests {
         flag++;
         return request.createResponseBuilder(HttpStatus.OK).body(String.valueOf(flag)).build();
     }
+
+    @FunctionName("HttpTriggerWaitMethod")
+    public HttpResponseMessage wait(
+            @HttpTrigger(name = "req", methods = {HttpMethod.GET, HttpMethod.POST}, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
+            final ExecutionContext context
+    ) {
+        context.getLogger().info("Java HTTP trigger processed a request.");
+
+        // Parse query parameters
+        String query = request.getQueryParameters().get("name");
+        String name = request.getBody().orElse(query);
+
+        if (name == null) {
+            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name on the query string or in the request body").build();
+        } else {
+            return request.createResponseBuilder(HttpStatus.OK).body(name).build();
+        }
+    }
+
+    @FunctionName("HttpTriggerNotifyMethod")
+    public HttpResponseMessage notify(
+            @HttpTrigger(name = "req", methods = {HttpMethod.GET, HttpMethod.POST}, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
+            final ExecutionContext context
+    ) {
+        context.getLogger().info("Java HTTP trigger processed a request.");
+
+        // Parse query parameters
+        String query = request.getQueryParameters().get("name");
+        String name = request.getBody().orElse(query);
+
+        if (name == null) {
+            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name on the query string or in the request body").build();
+        } else {
+            return request.createResponseBuilder(HttpStatus.OK).body(name).build();
+        }
+    }
 }
