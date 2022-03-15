8fa0c762119b920abb3a0f6c8d209432872de08d
Shreyas Gopalakrishna <shreyasg@microsoft.com>
2022-03-15 12:12:21 -0500

    Downloading application insights agent as part of the java worker
    Downloading application insights agent as part of the java worker
    
    Updated setup-tests-pipeline.ps1 to unsign app insights jar
    
    Update package-pipeline.ps1
    
    Update setup-tests-pipeline.ps1

--- setup-tests-pipeline.ps1
+++ setup-tests-pipeline.ps1
@@ -29,6 +29,10 @@ else
 
 $FUNC_CLI_DIRECTORY = Join-Path $PSScriptRoot 'Azure.Functions.Cli'
 
+$ApplicationInsightsAgentVersion = '3.2.8'
+$ApplicationInsightsAgentFilename = "applicationinsights-agent-${ApplicationInsightsAgentVersion}.jar"
+$ApplicationInsightsAgentUrl = "https://repo1.maven.org/maven2/com/microsoft/azure/applicationinsights-agent/${ApplicationInsightsAgentVersion}/${ApplicationInsightsAgentFilename}"
+
 Write-Host 'Deleting the Core Tools if exists...'
 Remove-Item -Force "$FUNC_CLI_DIRECTORY.zip" -ErrorAction Ignore
 Remove-Item -Recurse -Force $FUNC_CLI_DIRECTORY -ErrorAction Ignore
@@ -54,5 +58,69 @@ if (-not $UseCoreToolsBuildFromIntegrationTests.IsPresent)
     Copy-Item "$PSScriptRoot/worker.config.json" "$FUNC_CLI_DIRECTORY/workers/java" -Force -Verbose
     Write-Host "Copying worker.config.json and annotationLib to worker directory"
     Copy-Item "$PSScriptRoot/annotationLib" "$FUNC_CLI_DIRECTORY/workers/java/annotationLib" -Recurse -Verbose
+    
+    # Download application insights agent from maven central
+    $ApplicationInsightsAgentFile = [System.IO.Path]::Combine($PSScriptRoot, $ApplicationInsightsAgentFilename)
 
+    # local testing cleanup
+    if (Test-Path -Path $ApplicationInsightsAgentFile) {
+        Remove-Item -Path $ApplicationInsightsAgentFile
+    }
+
+    # local testing cleanup
+    $oldOutput = [System.IO.Path]::Combine($PSScriptRoot, "agent")
+    if (Test-Path -Path $oldOutput) {
+        Remove-Item -Path $oldOutput -Recurse
+    }
+
+    # local testing cleanup
+    $oldExtract = [System.IO.Path]::Combine($PSScriptRoot, "extract")
+    if (Test-Path -Path $oldExtract) {
+        Remove-Item -Path $oldExtract -Recurse
+    }
+    
+    $extract = new-item -type directory -force $PSScriptRoot\extract
+    if (-not(Test-Path -Path $extract)) { 
+        echo "Fail to create a new directory $extract"
+        exit 1
+    }
+
+    echo "Start extracting content from $ApplicationInsightsAgentFilename to extract folder"
+    cd -Path $extract -PassThru
+    jar xf $ApplicationInsightsAgentFile
+    cd $PSScriptRoot
+    echo "Done extracting"
+
+    echo "Unsign $ApplicationInsightsAgentFilename"
+    Remove-Item $extract\META-INF\MSFTSIG.*
+    $manifest = "$extract\META-INF\MANIFEST.MF"
+    $newContent = (Get-Content -Raw $manifest | Select-String -Pattern '(?sm)^(.*?\r?\n)\r?\n').Matches[0].Groups[1].Value
+    Set-Content -Path $manifest $newContent
+
+    Remove-Item $ApplicationInsightsAgentFile
+    if (-not(Test-Path -Path $ApplicationInsightsAgentFile)) { 
+        echo "Delete the original $ApplicationInsightsAgentFilename successfully"
+    } else {
+        echo "Fail to delete original source $ApplicationInsightsAgentFilename"
+        exit 1
+    }
+    
+    $agent = new-item -type directory -force $PSScriptRoot\agent
+    $filename = "applicationinsights-agent.jar"
+    $result = [System.IO.Path]::Combine($agent, $filename)
+    echo "re-jar $filename"
+
+    cd -Path $extract -PassThru
+    jar cfm $result META-INF/MANIFEST.MF .
+
+    if (-not(Test-Path -Path $result)) { 
+        echo "Fail to re-archive $filename"
+        exit 1
+    }
+    
+    Write-Host "Creating the functions.codeless file"
+    New-Item -path $PSScriptRoot\agent -type file -name "functions.codeless"
+        
+    Write-Host "Copying the unsigned Application Insights Agent to worker directory"
+    Copy-Item "$PSScriptRoot/agent" "$FUNC_CLI_DIRECTORY/workers/java/agent" -Recurse -Verbose
 }
