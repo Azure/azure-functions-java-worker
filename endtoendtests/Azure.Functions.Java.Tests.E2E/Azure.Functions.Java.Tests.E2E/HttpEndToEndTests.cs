875e73a930f2625a04b0952c6363126043f20e1b
kaibocai <89094811+kaibocai@users.noreply.github.com>
2022-08-16 13:20:02 -0500

    testing java home

--- endtoendtests/Azure.Functions.Java.Tests.E2E/Azure.Functions.Java.Tests.E2E/HttpEndToEndTests.cs
+++ endtoendtests/Azure.Functions.Java.Tests.E2E/Azure.Functions.Java.Tests.E2E/HttpEndToEndTests.cs
@@ -19,52 +19,52 @@ namespace Azure.Functions.Java.Tests.E2E
         }
 
         // TODO: remove commented test cases
-        [Theory]
-        [InlineData("HttpTriggerJava", "?&name=Test", HttpStatusCode.OK, "Test")]
-        [InlineData("FontTypeSupport", "?&name=Test", HttpStatusCode.OK, "Test")]
-        [InlineData("HttpTriggerJavaMetadata", "?&firstName=John&lastName=Doe", HttpStatusCode.OK, "JohnDoe")]
-        [InlineData("HttpTriggerJavaThrows", "", HttpStatusCode.InternalServerError, "")]
-        [InlineData("HttpTriggerJava", "", HttpStatusCode.BadRequest, "Please pass a name on the query string or in the request body")]
-        //[InlineData("HttpExample-runRetryFail", "", HttpStatusCode.InternalServerError, "")]
-        //[InlineData("HttpExample-runExponentialBackoffRetryFail",  "", HttpStatusCode.InternalServerError, "")]
-        [InlineData("HttpTriggerWaitMethod", "?&name=Test", HttpStatusCode.OK, "Test")]
-        [InlineData("HttpTriggerNotifyMethod", "?&name=Test", HttpStatusCode.OK, "Test")]
+//        [Theory]
+//        [InlineData("HttpTriggerJava", "?&name=Test", HttpStatusCode.OK, "Test")]
+//        [InlineData("FontTypeSupport", "?&name=Test", HttpStatusCode.OK, "Test")]
+//        [InlineData("HttpTriggerJavaMetadata", "?&firstName=John&lastName=Doe", HttpStatusCode.OK, "JohnDoe")]
+//        [InlineData("HttpTriggerJavaThrows", "", HttpStatusCode.InternalServerError, "")]
+//        [InlineData("HttpTriggerJava", "", HttpStatusCode.BadRequest, "Please pass a name on the query string or in the request body")]
+//        //[InlineData("HttpExample-runRetryFail", "", HttpStatusCode.InternalServerError, "")]
+//        //[InlineData("HttpExample-runExponentialBackoffRetryFail",  "", HttpStatusCode.InternalServerError, "")]
+//        [InlineData("HttpTriggerWaitMethod", "?&name=Test", HttpStatusCode.OK, "Test")]
+//        [InlineData("HttpTriggerNotifyMethod", "?&name=Test", HttpStatusCode.OK, "Test")]
         [InlineData("HttpTriggerJavaVersion", "", HttpStatusCode.OK, "HttpTriggerJavaVersion")]
-        [InlineData("echo", "?&name=springcloudfunctionecho", HttpStatusCode.OK, "springcloudfunctionecho")]
-        [InlineData("uppercase", "?&name=springcloudfunctionuppercase", HttpStatusCode.OK, "SPRINGCLOUDFUNCTIONUPPERCASE")]
+//        [InlineData("echo", "?&name=springcloudfunctionecho", HttpStatusCode.OK, "springcloudfunctionecho")]
+//        [InlineData("uppercase", "?&name=springcloudfunctionuppercase", HttpStatusCode.OK, "SPRINGCLOUDFUNCTIONUPPERCASE")]
         public async Task HttpTriggerTests(string functionName, string queryString, HttpStatusCode expectedStatusCode, string expectedErrorMessage)
         {
             // TODO: Verify exception on 500 after https://github.com/Azure/azure-functions-host/issues/3589
             Assert.True(await Utilities.InvokeHttpTrigger(functionName, queryString, expectedStatusCode, expectedErrorMessage));
         }
 
-        [Fact]
-        public async Task HttpTrigger_ReturnsCustomCode()
-        {
-            Assert.True(await Utilities.InvokeHttpTrigger("HttpTriggerCustomCode", "?&name=Test",  HttpStatusCode.OK, "Test", 209));
-        }
-
-        [Fact]
-        public async Task HttpTriggerJavaClassLoader()
-        {
-
-            Assert.True(await Utilities.InvokeHttpTrigger("HttpTriggerJavaClassLoader", "?&name=Test", HttpStatusCode.OK, "Test"));
-
-        }
-
-        [Fact]
-        public async void HttpTriggerJavaStatic()
-        {
-            await HttpTriggerTests("HttpTriggerJavaStatic1", "", HttpStatusCode.OK, "1");
-            await HttpTriggerTests("HttpTriggerJavaStatic2", "", HttpStatusCode.OK, "2");
-            await HttpTriggerTests("HttpTriggerJavaStatic1", "", HttpStatusCode.OK, "3");
-            await HttpTriggerTests("HttpTriggerJavaStatic2", "", HttpStatusCode.OK, "4");
-        }
-
-        [Fact]
-        public async Task HttpTrigger_BindingName()
-        {
-            Assert.True(await Utilities.InvokeHttpTrigger("BindingName", "/testMessage", HttpStatusCode.OK, "testMessage"));
-        }
+//        [Fact]
+//        public async Task HttpTrigger_ReturnsCustomCode()
+//        {
+//            Assert.True(await Utilities.InvokeHttpTrigger("HttpTriggerCustomCode", "?&name=Test",  HttpStatusCode.OK, "Test", 209));
+//        }
+//
+//        [Fact]
+//        public async Task HttpTriggerJavaClassLoader()
+//        {
+//
+//            Assert.True(await Utilities.InvokeHttpTrigger("HttpTriggerJavaClassLoader", "?&name=Test", HttpStatusCode.OK, "Test"));
+//
+//        }
+//
+//        [Fact]
+//        public async void HttpTriggerJavaStatic()
+//        {
+//            await HttpTriggerTests("HttpTriggerJavaStatic1", "", HttpStatusCode.OK, "1");
+//            await HttpTriggerTests("HttpTriggerJavaStatic2", "", HttpStatusCode.OK, "2");
+//            await HttpTriggerTests("HttpTriggerJavaStatic1", "", HttpStatusCode.OK, "3");
+//            await HttpTriggerTests("HttpTriggerJavaStatic2", "", HttpStatusCode.OK, "4");
+//        }
+//
+//        [Fact]
+//        public async Task HttpTrigger_BindingName()
+//        {
+//            Assert.True(await Utilities.InvokeHttpTrigger("BindingName", "/testMessage", HttpStatusCode.OK, "testMessage"));
+//        }
     }
 }
