a65863f170c7b47b275c5b4a393e826ef43a8e36
kaibocai <89094811+kaibocai@users.noreply.github.com>
2022-08-31 12:11:30 -0700

    create FunctionClassLoaderProvider at each test

--- src/test/java/com/microsoft/azure/functions/worker/broker/JavaMethodExecutorTest.java
+++ src/test/java/com/microsoft/azure/functions/worker/broker/JavaMethodExecutorTest.java
@@ -6,6 +6,8 @@ import org.junit.*;
 import static junit.framework.TestCase.*;
 
 import java.io.File;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.*;
 
 
@@ -15,7 +17,7 @@ import com.microsoft.azure.functions.worker.reflect.*;
 
 public class JavaMethodExecutorTest {
 
-	private static FunctionClassLoaderProvider functionClassLoaderProvider = new FunctionClassLoaderProvider();
+//	private static FunctionClassLoaderProvider functionClassLoaderProvider = new FunctionClassLoaderProvider();
 
 	//TODO: figure out why below logics not always works on azure devops pipelines, it seems more to be a issue from
 	//	azure devops pipelines. Currently, all test cases are testing the TestFunctionsClass using system classloader as
@@ -31,11 +33,26 @@ public class JavaMethodExecutorTest {
 //			throw new RuntimeException("TestFunctionsClass.jar not exist");
 //		}
 //		functionClassLoaderProvider.addCustomerUrl(testJar.toURI().toURL());
+//		URL[] urls = new URL[1];
+//		urls[0] = testJar.toURI().toURL();
+//		URLClassLoader urlClassLoader = new URLClassLoader(urls);
+//		Class<?> aClass = Class.forName("com.microsoft.azure.functions.worker.broker.tests.TestFunctionsClass", false, urlClassLoader);
+//		Object o = aClass.newInstance();
+//		aClass.getDeclaredMethod()
 //	}
 
 	@Test
     public void functionMethodLoadSucceeds() throws Exception {
-		FunctionMethodDescriptor descriptor = new FunctionMethodDescriptor("testid", "TestHttpTrigger","com.microsoft.azure.functions.worker.broker.TestFunctionsClass.TestHttpTrigger","TestFunctionsClass.jar");
+		String targetPath = JavaMethodExecutorTest.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+		File testJar = new File(targetPath + "/TestFunctionsClass.jar");
+		boolean exists = testJar.exists();
+		if (!exists) {
+			WorkerLogManager.getSystemLogger().severe(testJar + "doesn't exist");
+			throw new RuntimeException("TestFunctionsClass.jar not exist");
+		}
+		FunctionClassLoaderProvider functionClassLoaderProvider = new FunctionClassLoaderProvider();
+		functionClassLoaderProvider.addCustomerUrl(testJar.toURI().toURL());
+		FunctionMethodDescriptor descriptor = new FunctionMethodDescriptor("testid", "TestHttpTrigger","com.microsoft.azure.functions.worker.broker.tests.TestFunctionsClass.TestHttpTrigger","TestFunctionsClass.jar");
 		Map<String, BindingInfo> bindings = new HashMap<>();
 		bindings.put("$return", BindingInfo.newBuilder().setDirection(BindingInfo.Direction.out).build());
 		JavaMethodExecutor executor = new FunctionMethodExecutorImpl(descriptor, bindings, functionClassLoaderProvider);
@@ -45,7 +62,16 @@ public class JavaMethodExecutorTest {
 	
 	@Test(expected = NoSuchMethodException.class)   
     public void functionMethodLoadFails_DoesnotExist() throws Exception {
-		FunctionMethodDescriptor descriptor = new FunctionMethodDescriptor("testid", "TestHttpTrigger1","com.microsoft.azure.functions.worker.broker.TestFunctionsClass.TestHttpTrigger1","TestFunctionsClass.jar");
+		String targetPath = JavaMethodExecutorTest.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+		File testJar = new File(targetPath + "/TestFunctionsClass.jar");
+		boolean exists = testJar.exists();
+		if (!exists) {
+			WorkerLogManager.getSystemLogger().severe(testJar + "doesn't exist");
+			throw new RuntimeException("TestFunctionsClass.jar not exist");
+		}
+		FunctionClassLoaderProvider functionClassLoaderProvider = new FunctionClassLoaderProvider();
+		functionClassLoaderProvider.addCustomerUrl(testJar.toURI().toURL());
+		FunctionMethodDescriptor descriptor = new FunctionMethodDescriptor("testid", "TestHttpTrigger1","com.microsoft.azure.functions.worker.broker.tests.TestFunctionsClass.TestHttpTrigger1","TestFunctionsClass.jar");
 		Map<String, BindingInfo> bindings = new HashMap<>();
 		bindings.put("$return", BindingInfo.newBuilder().setDirection(BindingInfo.Direction.out).build());
 		JavaMethodExecutor executor = new FunctionMethodExecutorImpl(descriptor, bindings, functionClassLoaderProvider);
@@ -53,7 +79,16 @@ public class JavaMethodExecutorTest {
 	
 	@Test(expected = UnsupportedOperationException.class)   
     public void functionMethodLoadFails_DuplicateAnnotations() throws Exception {
-		FunctionMethodDescriptor descriptor = new FunctionMethodDescriptor("testid", "TestHttpTriggerOverload","com.microsoft.azure.functions.worker.broker.TestFunctionsClass.TestHttpTriggerOverload","TestFunctionsClass.jar");
+		String targetPath = JavaMethodExecutorTest.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+		File testJar = new File(targetPath + "/TestFunctionsClass.jar");
+		boolean exists = testJar.exists();
+		if (!exists) {
+			WorkerLogManager.getSystemLogger().severe(testJar + "doesn't exist");
+			throw new RuntimeException("TestFunctionsClass.jar not exist");
+		}
+		FunctionClassLoaderProvider functionClassLoaderProvider = new FunctionClassLoaderProvider();
+		functionClassLoaderProvider.addCustomerUrl(testJar.toURI().toURL());
+		FunctionMethodDescriptor descriptor = new FunctionMethodDescriptor("testid", "TestHttpTriggerOverload","com.microsoft.azure.functions.worker.broker.tests.TestFunctionsClass.TestHttpTriggerOverload","TestFunctionsClass.jar");
 		Map<String, BindingInfo> bindings = new HashMap<>();
 		bindings.put("$return", BindingInfo.newBuilder().setDirection(BindingInfo.Direction.out).build());
 		JavaMethodExecutor executor = new FunctionMethodExecutorImpl(descriptor, bindings, functionClassLoaderProvider);
