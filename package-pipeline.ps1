7f1886cf108b09437297820fa34e469d317f3740
kaibocai <89094811+kaibocai@users.noreply.github.com>
2022-11-15 09:36:07 -0600

    update distribute agent version to 3.4.4

--- package-pipeline.ps1
+++ package-pipeline.ps1
@@ -10,7 +10,7 @@ function StopOnFailedExecution {
   }
 }
 
-$ApplicationInsightsAgentVersion = '3.4.0'
+$ApplicationInsightsAgentVersion = '3.4.4'
 $ApplicationInsightsAgentFilename = "applicationinsights-agent-${ApplicationInsightsAgentVersion}.jar"
 $ApplicationInsightsAgentUrl = "https://repo1.maven.org/maven2/com/microsoft/azure/applicationinsights-agent/${ApplicationInsightsAgentVersion}/${ApplicationInsightsAgentFilename}"
 
