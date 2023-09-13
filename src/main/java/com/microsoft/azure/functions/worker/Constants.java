39b4af0151ded406d51f15fe3372a33741be805c
Shreyas Gopalakrishna <shreyasg@microsoft.com>
2023-09-13 18:59:29 -0500

    Adding support for updated command line args

--- src/main/java/com/microsoft/azure/functions/worker/Constants.java
+++ src/main/java/com/microsoft/azure/functions/worker/Constants.java
@@ -5,6 +5,12 @@ package com.microsoft.azure.functions.worker;
  */
 public final class Constants {
     private Constants(){}
+
+    public final static String FUNCTIONS_URI_OPTION = "functions-uri";
+    public final static String FUNCTIONS_WORKER_ID_OPTION = "functions-worker-id";
+    public final static String FUNCTIONS_REQUEST_ID_OPTION = "functions-request-id";
+    public final static String FUNCTIONS_GRPC_MAX_MESSAGE_LENGTH_OPTION = "functions-grpc-max-message-length";
+    public final static String FUNCTIONS_CONSOLE_LOG_OPTION = "functions-console-log";
     public final static String TRIGGER_METADATA_DOLLAR_REQUEST_KEY = "$request";
     public final static String JAVA_LIBRARY_DIRECTORY = "/annotationLib";
     public final static String JAVA_LIBRARY_ARTIFACT_ID = "azure-functions-java-library";
