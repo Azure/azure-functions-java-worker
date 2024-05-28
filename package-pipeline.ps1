4c5584cf0ea31383a6add1f8855475e643881f8b
Shreyas Gopalakrishna <11889130+shreyas-gopalakrishna@users.noreply.github.com>
2024-05-28 14:09:01 -0700

    Update package-pipeline.ps1

--- package-pipeline.ps1
+++ package-pipeline.ps1
@@ -10,7 +10,7 @@ function StopOnFailedExecution {
   }
 }
 
-$ApplicationInsightsAgentVersion = '3.5.1'
+$ApplicationInsightsAgentVersion = '3.5.2'
 $ApplicationInsightsAgentFilename = "applicationinsights-agent-${ApplicationInsightsAgentVersion}.jar"
 $ApplicationInsightsAgentUrl = "https://repo1.maven.org/maven2/com/microsoft/azure/applicationinsights-agent/${ApplicationInsightsAgentVersion}/${ApplicationInsightsAgentFilename}"
 
