b746ff48f1471370bc022b1988fd233068c7f124
Ahmed Muhsin <ahmedmuhsin@microsoft.com>
2025-01-30 11:48:02 -0600

    skip removing signature from appins agent

--- package-pipeline.ps1
+++ package-pipeline.ps1
@@ -41,42 +41,46 @@ if (Test-Path -Path $oldOutput) {
     Remove-Item -Path $oldOutput -Recurse
 }
 
-# local testing cleanup
-$oldExtract = [System.IO.Path]::Combine($PSScriptRoot, "extract")
-if (Test-Path -Path $oldExtract) {
-    Remove-Item -Path $oldExtract -Recurse
-}
-
-$extract = new-item -type directory -force $PSScriptRoot\extract
-if (-not(Test-Path -Path $extract)) {
-    echo "Fail to create a new directory $extract"
-    exit 1
-}
-
-echo "Start extracting content from $ApplicationInsightsAgentFilename to extract folder"
-cd -Path $extract -PassThru
-jar xf $ApplicationInsightsAgentFile
-cd $PSScriptRoot
-echo "Done extracting"
-
-echo "Unsign $ApplicationInsightsAgentFilename"
-Remove-Item $extract\META-INF\MSFTSIG.*
-$manifest = "$extract\META-INF\MANIFEST.MF"
-$newContent = (Get-Content -Raw $manifest | Select-String -Pattern '(?sm)^(.*?\r?\n)\r?\n').Matches[0].Groups[1].Value
-Set-Content -Path $manifest $newContent
+## local testing cleanup
+#$oldExtract = [System.IO.Path]::Combine($PSScriptRoot, "extract")
+#if (Test-Path -Path $oldExtract) {
+#    Remove-Item -Path $oldExtract -Recurse
+#}
+#
+#$extract = new-item -type directory -force $PSScriptRoot\extract
+#if (-not(Test-Path -Path $extract)) {
+#    echo "Fail to create a new directory $extract"
+#    exit 1
+#}
+#
+#echo "Start extracting content from $ApplicationInsightsAgentFile to extract folder"
+#cd -Path $extract -PassThru
+#jar xf $ApplicationInsightsAgentFile
+#cd $PSScriptRoot
+#echo "Done extracting"
+#
+#echo "Unsign $ApplicationInsightsAgentFile"
+#Remove-Item $extract\META-INF\MSFTSIG.*
+#$manifest = "$extract\META-INF\MANIFEST.MF"
+#$newContent = (Get-Content -Raw $manifest | Select-String -Pattern '(?sm)^(.*?\r?\n)\r?\n').Matches[0].Groups[1].Value
+#Set-Content -Path $manifest $newContent
 
 $agent = new-item -type directory -force $PSScriptRoot\agent
 $filename = "applicationinsights-agent.jar"
-$result = [System.IO.Path]::Combine($agent, $filename)
-echo "re-jar $filename"
+#$result = [System.IO.Path]::Combine($agent, $filename)
+#echo "re-jar $filename"
+#
+#cd -Path $extract -PassThru
+#jar cfm $result META-INF/MANIFEST.MF .
+#
+#if (-not(Test-Path -Path $result)) {
+#    echo "Fail to re-archive $filename"
+#    exit 1
+#}
 
-cd -Path $extract -PassThru
-jar cfm $result META-INF/MANIFEST.MF .
+Copy-Item $ApplicationInsightsAgentFile -Destination (Join-Path $agent $filename)
+Write-Host "Agent copied successfully to $agentDir"
 
-if (-not(Test-Path -Path $result)) {
-    echo "Fail to re-archive $filename"
-    exit 1
-}
 Write-Host "Creating the functions.codeless file"
 New-Item -path $PSScriptRoot\agent -type file -name "functions.codeless"
 
